0a590c6eaed30c5859c46b85b96aff8b0c13ed29
Ricky Fernando <rickyfernandoacong@gmail.com>
2019-11-25 12:24:27 +0700

    change arithmetiic

--- arithmetic.pl
+++ arithmetic.pl
@@ -1,14 +1,14 @@
 
 operator1 -->
-    (blank,space),(variable;numbers),((space;blank),("+";"-";"/";"*";"**";"//"),(blank;space),(minus;blank)),(operator1;blank).
+    (blank,space),(minusPlus;blank),(variable;numbers),((space;blank),("+";"-";"/";"*";"**";"//";"%";"&";"|";"^";"~";"<<";">>"),(blank;space),(minusPlus;blank)),(operator1;blank).
 
 operator -->
     operator1,(variable;numbers),(blank;space).
 
-operatormin -->
-    "-".
-minus -->
-    operatormin;(operatormin;blank;space),(minus,blank).
+operatorminplus -->
+    "-","+".
+minusPlus -->
+    operatormin;(operatormin;blank;space),(minusPlus;blank).
 
 equal -->
     "=".
