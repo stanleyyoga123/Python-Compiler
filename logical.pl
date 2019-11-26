b0c60e83b38e6b9aaeba6b99b19d439985399494
stanleyyoga123 <yoga360227@gmail.com>
2019-11-26 17:25:34 +0700

    if else upgraded version hehe

--- logical.pl
+++ logical.pl
@@ -28,16 +28,25 @@ bracket -->
 
 inside -->
     (blank;class;fungsi;imports;importsAs;importsFrom;importsFromAs;input;isitotal;komentar;looptotal;raise;assign);
-    (blank;class;fungsi;imports;importsAs;importsFrom;importsFromAs;input;isitotal;komentar;looptotal;raise;assign),(space;"\n"),inside.
+    (blank;class;fungsi;imports;importsAs;importsFrom;importsFromAs;input;isitotal;komentar;looptotal;raise;assign),(space;"\n";blank),inside.
 
 conditionalElse -->
     blank;
     ("elif",(space;blank),bracket,(space;blank),":","\n",inside);
+    ("elif",(space;blank),bracket,(space;blank),":","\n",rtn);
+    
     ("else",(space;blank),":","\n",inside);
-    ("elif",(space;blank),bracket,(space;blank),":","\n",(inside,"\n",rtn),conditionalElse);
+    ("else",(space;blank),":","\n",rtn);
+    
+    ("elif",(space;blank),bracket,":","\n",conditionalElse);
     ("elif",(space;blank),bracket,(space;blank),":","\n",inside,conditionalElse);
-    ("else",(space;blank),bracket,(space;blank),":","\n",(inside,"\n",rtn),conditionalElse);
-    ("else",(space;blank),":","\n",inside,conditionalElse).
+    ("elif",(space;blank),bracket,(space;blank),":","\n",rtn,conditionalElse);
+    ("elif",(space;blank),bracket,(space;blank),":","\n",(inside,"\n",rtn),conditionalElse);
+    
+    ("else",(space;blank),":","\n",conditionalElse);
+    ("else",(space;blank),":","\n",inside,conditionalElse);
+    ("else",(space;blank),":","\n",rtn,conditionalElse);
+    ("else",(space;blank),":","\n",(inside,"\n",rtn),conditionalElse).
 
 conditional -->
     blank;
